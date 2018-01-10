96c267b8a3b068b22164014bea7d8066b0f7902e
gustavo71 <registerguus@gmail.com>
2018-01-10 16:18:00 +0100

    Created HTTP calls

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,25 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../src/index.html'));
 })
 
+app.get('/users', function(req, res){
+  res.json([
+    {"id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com"},
+    {"id": 2, "firstName": "Tammy", "lastName": "Norton", "email": "tnorton@yahoo.com"},
+    {"id": 3, "firstName": "Tina", "lastName": "Lee", "email": "lee.tina@hotmail.com"}
+  ])
+})
+
+app.get('/otherUsers', function(req, res){
+  res.json([
+    {"id": 1, "firstName": "Jan", "lastName": "Janssen", "email": "jan@gmail.com"},
+    {"id": 2, "firstName": "Henk", "lastName": "de Vries", "email": "hdevries@yahoo.com"},
+    {"id": 3, "firstName": "Piet", "lastName": "de Jong", "email": "jong.pietde@hotmail.com"}
+  ])
+})
+
+
+
+
 app.listen(port, function(err){
   if (err) {
     console.log(err);
