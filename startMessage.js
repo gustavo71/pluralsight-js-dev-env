c950bd8c7339da885fbe946b1502fe0e6b62da2f
gustavo71 <35226687+gustavo71@users.noreply.github.com>
2018-01-10 13:04:53 +0100

    Delete startMessage.js

--- startMessage.js
+++
@@ -1,4 +0,0 @@
-//var chalk = require('chalk');
-import chalk from 'chalk';  //ES6 - Transpiled by Babel (see package.json)
-
-console.log(chalk.green('Starting app in dev mode ...')); // eslint-disable-line no-console
