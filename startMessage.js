57d2f8ab5de4e371488996485e3c6ef2ed7a7dd5
gustavo71 <35226687+gustavo71@users.noreply.github.com>
2018-01-10 12:53:14 +0100

    New files

---
+++ startMessage.js
@@ -0,0 +1,4 @@
+//var chalk = require('chalk');
+import chalk from 'chalk';  //ES6 - Transpiled by Babel (see package.json)
+
+console.log(chalk.green('Starting app in dev mode ...')); // eslint-disable-line no-console
